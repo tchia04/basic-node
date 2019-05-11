3730610aaead69d371ed42974335d32f74c84e8e
Tony Chia <tchia04@gmail.com>
2019-05-11 09:08:20 -0700

    make a test fail

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -3,12 +3,12 @@ var request = require('supertest');
 
 describe('/users route',
   function () {
-    describe('GET /users',
+    describe('GET /users_x',
       function () {
         it('should respond with content',
           function (done) {
             request(app)
-              .get('/users')
+              .get('/users_x')
               .expect('Content-Type', /text\/html/)
               .expect(200, done);
           }
